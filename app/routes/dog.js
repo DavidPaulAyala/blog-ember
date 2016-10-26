a7fa021b2637ce7a54c0dd5ab434b9fca5e9c726
Alvaro Flores and David Ayala <alvaro@>
2016-10-26 15:10:11 -0700

    added some dynamic routing

---
+++ app/routes/dog.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model(params) {
+    return this.store.findRecord('dog', params.dog_id);
+  },
+  actions: {
+    update(dog, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          dog.set(key,params[key]);
+        }
+      });
+      dog.save();
+      this.transitionTo('index');
+    },
+
+    deleteDog(dog){
+      dog.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
+});
